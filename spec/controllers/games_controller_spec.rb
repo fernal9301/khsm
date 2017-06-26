2043de633ba47b93e17e954c2d6aba974e5b40f7
Sergey Ivanov <ivanovsergey1988@gmail.com>
2017-06-26 12:31:38 +0000

    homework-62-3

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -84,5 +84,21 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(user_path(user))
       expect(flash[:warning]).to be
     end
+
+    # homework-62-3
+    it 'try to create second game' do
+      # убедились что есть игра в работе
+      expect(game_w_questions.finished?).to be_falsey
+
+      # отправляем запрос на создание, убеждаемся что новых Game не создалось
+      expect { post :create }.to change(Game, :count).by(0)
+
+      game = assigns(:game) # вытаскиваем из контроллера поле @game
+      expect(game).to be_nil
+
+      # и редирект на страницу старой игры
+      expect(response).to redirect_to(game_path(game_w_questions))
+      expect(flash[:alert]).to be
+    end
   end
 end
