31db0b697de21ed48b65125bea4004bf04495d32
Sergey Ivanov <ivanovsergey1988@gmail.com>
2017-06-26 07:04:01 +0000

    homework-62-1

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -47,6 +47,17 @@ RSpec.describe GamesController, type: :controller do
       expect(response.status).to eq(200) # должен быть ответ HTTP 200
       expect(response).to render_template('show') # и отрендерить шаблон show
     end
+
+    # homework-62-1
+    it 'not #show alien game' do 
+      alien_game = FactoryGirl.create(:game_with_questions)
+      
+      get :show, id: alien_game.id 
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to be
+    end 
+ 
     # юзер отвечает на игру корректно - игра продолжается
     it 'answers correct' do
       # передаем параметр params[:letter]
