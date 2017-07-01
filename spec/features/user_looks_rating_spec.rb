8db56e218789ba4c1403c3338f27fe2815aec5f6
fernal9301 <ivanovsergey1988@gmail.com>
2017-07-01 08:45:05 +0300

    edit homework-65-1 add ! to let

--- spec/features/user_looks_rating_spec.rb
+++ spec/features/user_looks_rating_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature 'USER looks game rating', type: :feature do
-  let(:user) { FactoryGirl.create(:user, name: 'Настя', balance: '100') }
+  let!(:user) { FactoryGirl.create(:user, name: 'Настя', balance: '100') }
 
 
   scenario 'successfully' do
     visit '/'
 
-    expect(page).to have_content 'Настя'
+    expect(page).to have_content "Настя"
     expect(page).to have_content '100'
     #save_and_open_page
   end
